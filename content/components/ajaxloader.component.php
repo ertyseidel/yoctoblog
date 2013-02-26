441a9b29da9f73bbc6c5f3710dc1d7ab11e89c1f
Erty Seidel <ertysdl+github@gmail.com>
2013-02-26 12:01:57 -0600

    Removed content file

--- content/components/ajaxloader.component.php
+++
@@ -1,9 +0,0 @@
-<?php
-	if(count($y->ajax) == 0) return;
-	echo('<script type="text/javascript" src="./js/mini.min.js"></script><script type="text/javascript">window.addEventListener(\'load\', function(){');
-	foreach($y->ajax as $ajax){
-		if($ajax['call'] == 'onload'){
-			echo("mini.ajax.update(\"{$ajax['source']}\", \"{$ajax['id']}\");");
-		}
-	}
-	echo('}, false);</script>');
--- content/templates/post.template.php
+++
@@ -1,4 +0,0 @@
-<h2 class="post-title"><?php echo $post->title ?></h2>
-<p class="post-detail">By <?php echo $post->author ?> on <?php echo $post->posttime('m/d/Y')?></p>
-<hr />
-<div class="post-content"><?php echo $post->content ?></div>
